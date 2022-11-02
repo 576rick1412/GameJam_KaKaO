9ba398dafa832aa2d6b3ecb9f526e6aef8d29a52
576rick1412 <576rick1412@gmail.com>
2022-11-02 12:17:59 +0900

    스테이지 버그 수정 및 뭔가 덕지덕지 추가

--- Assets/1. CS/InGame/All_In_One.cs
+++ Assets/1. CS/InGame/All_In_One.cs
@@ -10,10 +10,9 @@ public class All_In_One : MonoBehaviour
     // 원활한 사용을 위해 모래 / 체크 오브젝트의 레이케스트 타겟을 꺼놔야함
     [SerializeField] float DistanceRange = 0; // 드롭 좌표 허용 범위
 
+    [SerializeField] int Check_Num = 0; //성공한 수
     [SerializeField] GameObject[] Image_Objects; //이동오브제
     [SerializeField] GameObject[] Set_Objects; // 빈칸
-    // [SerializeField] GameObject[] SetImage; // 빈칸에 놓으면 나오는 이미지
-    [SerializeField] int Check_Num = 0; //성공한 수
     [SerializeField] Vector3[] target_POS;
 
     RectTransform rect;
@@ -27,10 +26,6 @@ public class All_In_One : MonoBehaviour
         {
             Array.Resize(ref target_POS, target_POS.Length + 1);
             target_POS[i] = Image_Objects[i].transform.position;
-
-            // 안보이게 해주는 마 ★ 법
-            // Image_Objects[i].SetActive(false);
-            // SetImage[i].SetActive(false);
         }
     }
     void Update()
@@ -49,15 +44,13 @@ public class All_In_One : MonoBehaviour
         float Distance = Vector3.Distance(Image_Objects[i].transform.position, Set_Objects[i].transform.position);
         if (Distance < DistanceRange)
         {
-            if (Image_Objects.Length <= Check_Num-1)
+            if (Image_Objects.Length - 1 <= Check_Num)
             {
                 // 마지막 조각을 놓았을 때
                 // 대충 이쯤에 클리어 창이 뜨도록 추가
                 Debug.Log("클리어");
             }
             Check_Num++;
-            // SetImage[i].SetActive(true); // ==================
-            //Destroy(Image_Objects[i]);
 
             Image_Objects[i].transform.position = Set_Objects[i].transform.position;
             return true;
--- Assets/1. CS/InGame/Ingame_Object.cs
+++ Assets/1. CS/InGame/Ingame_Object.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ingame_Object : MonoBehaviour
 {
+    // 캔버스에서 해당 오브젝트의 이미지 넘버를 입력
     [SerializeField] int object_Number;
+
+    // 제 위치에 놓을 시 참이 되며, 드래그가 불가능 하도록
     [SerializeField] bool AllowDrag;
 
-    public void OnDrag() { if (!AllowDrag) All_In_One.ALO.Drag_Image(object_Number); }
-    public void OnDrop() { AllowDrag = All_In_One.ALO.Drop_Image(object_Number); }
+    Image Image;
+    private void Start() { Image = GetComponent<Image>(); }
+
+    private void Update() { if (AllowDrag) Image.raycastTarget = false; }  // 드래그중 다른 오브젝트가 눌리지 않도록
+
+    public void OnDrag() { if (AllowDrag) return; All_In_One.ALO.Drag_Image(object_Number); }
+    public void OnDrop() { if (AllowDrag) return; AllowDrag = All_In_One.ALO.Drop_Image(object_Number); }
 }
