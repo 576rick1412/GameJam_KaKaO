9c9ef3846ca46de7cb8f017b4f3673700b2793fc
576rick1412 <576rick1412@gmail.com>
2022-11-02 12:54:47 +0900

    페이드 효과

--- Assets/1. CS/InGame/GameManager.cs
+++ Assets/1. CS/InGame/GameManager.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     static public GameManager instance { get; private set; }
     public bool isClear;
     public int clearIdx;
+
+    //페이드 인 아웃
+    [Header("페이드 인 아웃")]
+    public Image Panel; // 검은색 화면을 꽉 채우는 이미지 / 필수
+    public float F_time = 0.2f; // 페이드 시간
+    float FM_time = 0f; // 건들지마!!
     void Awake()
     {
         instance = this;
@@ -16,5 +23,110 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Fade()
+    {
+        StartCoroutine(FadeEffect());
+    }
+    public void Fade(GameObject CurObj, GameObject NextObj)
+    {
+        StartCoroutine(FadeEffect(CurObj, NextObj));
+    }
+    public void Fade(AsyncOperation op)
+    {
+        StartCoroutine(FadeEffect(op));
+    }
+
+    // ===========================================================================
+
+    IEnumerator FadeEffect()
+    {
+        Panel.gameObject.SetActive(true);
+        FM_time = 0f;
+        Color alpha = Panel.color;
+
+        while (alpha.a < 1f)
+        {
+            FM_time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(0, 1, FM_time);
+            Panel.color = alpha;
+
+            yield return null;
+        }
+
+        FM_time = 0f;
+        yield return null;
+
+
+        while (alpha.a > 0f)
+        {
+            FM_time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(1, 0, FM_time);
+            Panel.color = alpha;
+            yield return null;
+        }
+        Panel.gameObject.SetActive(false);
+        yield return null;
+    }
+    IEnumerator FadeEffect(GameObject CurObj, GameObject NextObj)
+    {
+        Panel.gameObject.SetActive(true);
+        FM_time = 0f;
+        Color alpha = Panel.color;
+
+        while (alpha.a < 1f)
+        {
+            FM_time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(0, 1, FM_time);
+            Panel.color = alpha;
+
+            yield return null;
+        }
+
+        FM_time = 0f;
+        CurObj.SetActive(false);
+        yield return null;  //yield return new WaitForSeconds(L_time);
+        NextObj.SetActive(true);
+
+
+        while (alpha.a > 0f)
+        {
+            FM_time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(1, 0, FM_time);
+            Panel.color = alpha;
+            yield return null;
+        }
+        Panel.gameObject.SetActive(false);
+        yield return null;
+    }
+    IEnumerator FadeEffect(AsyncOperation op)
+    {
+        Panel.gameObject.SetActive(true);
+        FM_time = 0f;
+        Color alpha = Panel.color;
+
+        while (alpha.a < 1f)
+        {
+            FM_time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(0, 1, FM_time);
+            Panel.color = alpha;
+
+            yield return null;
+        }
+
+        FM_time = 0f;
+        op.allowSceneActivation = true;
+        yield return null;   //yield return new WaitForSeconds(L_time);
+
+        while (alpha.a > 0f)
+        {
+            FM_time += Time.deltaTime / F_time;
+            alpha.a = Mathf.Lerp(1, 0, FM_time);
+            Panel.color = alpha;
+            yield return null;
+        }
+        Panel.gameObject.SetActive(false);
+        yield return null;
     }
 }
