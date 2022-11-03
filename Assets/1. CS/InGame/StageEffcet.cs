957de01623e3fc83a86ac2f7ee229cec07a01f87
kgoon <ehdwls4938@naver.com>
2022-11-03 09:56:54 +0900

    ClickStage

--- Assets/1. CS/InGame/StageEffcet.cs
+++ Assets/1. CS/InGame/StageEffcet.cs
@@ -1,23 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using DG.Tweening;
 
 public class StageEffcet : MonoBehaviour
 {
+    [SerializeField] private GameObject clearPanel;
+
     [SerializeField] private GameObject[] spawnObject;
-    [SerializeField] private int maxCount;
     [SerializeField] private GameObject hamerObjcet;
+
+    [SerializeField] private int maxCount;
+    private int clearCount;
+    private bool isClear;
     void Start()
     {
         StartCoroutine(FadeInOut());
-        for (int i = GameManager.instance.clearIdx;i<maxCount;i++)
+        clearCount = GameManager.instance.clearIdx;
+        SpawnObejct();
+    }
+    private void Update()
+    {
+        ClickStage();
+    }
+    void ClickStage()
+    {
+        if (!isClear && Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == spawnObject[clearCount])
+                    SceneManager.LoadScene("InGame");
+            }
+        }
+    }
+    void SpawnObejct()
+    {
+        for (int i = clearCount; i < maxCount; i++)
         {
             spawnObject[i].SetActive(true);
         }
-        if (GameManager.instance.isClear == false)
+        if (GameManager.instance.isClear == true)
         {
             StartCoroutine(HamerSwing());
+            if(clearCount++ >= 4)
+            {
+                isClear = true;
+            }
         }
     }
     private IEnumerator FadeInOut()
@@ -29,10 +61,5 @@ public class StageEffcet : MonoBehaviour
     {
         yield return new WaitForSeconds(2f);
         hamerObjcet.transform.DORotate(new Vector3(0, 180, 0), 1f);
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
